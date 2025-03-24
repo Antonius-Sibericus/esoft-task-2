e1a31c9c63c1da642849cb24832dcfe2db35bccd
Anton Kiosia <kesya273@gmail.com>
2025-03-24 15:27:44 +0500

    finish

--- copy.js
+++ copy.js
@@ -1,9 +1,23 @@
-const object = {
+const oldObject = {
     array: [
         1, 2, 3
     ],
+    bigArray: [
+        [11, 12, 13],
+        [21, 22, 23],
+        [31, 32, 33]
+    ],
     variable: "yellow",
-    someDate: new Date(2024-12-31)
+    someDate: new Date(2024-12-31),
+    someObject: {
+        secondVariable: "red"
+    },
+    set: new Set([1, 2, 3]),
+    map: new Map([[11, 22, 33]])
 }
 
-console.log(object.someDate);
+const createNewObj = (object) => {
+    return structuredClone(object);
+};
+
+console.log(createNewObj(oldObject).map);
